e0286a3171957d2e7e42508660868c6263dfa532
JimboCoolinsPP <jimmyc41018@gmail.com>
2026-05-11 12:12:32 -0400

    Add function to create and store a post

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function addPost() {
 
+  const title = document.getElementById("title").value;
+  const content = document.getElementById("content").value;
+
+  const post = {
+    title: title,
+    content: content,
+    date: new Date().toLocaleDateString()
+  };
+
+  let posts = JSON.parse(localStorage.getItem("posts")) || [];
+
+  posts.unshift(post);
+
+  localStorage.setItem("posts", JSON.stringify(posts));
+
+  alert("Post Added!");
+
+}
